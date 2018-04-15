85773e56cfd2c195a5405eb712607b3dda4e7cd0
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 23:11:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,6 @@ def badge_maker(name)
 end
 def batch_badge_creator(speakers)
   newarray = []
-  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   speakers.each do |makeabadge|
     makeabadge = badge_maker(name)
     newarray.push = (makeabadge)
