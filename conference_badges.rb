2015959b598716bea81bd3a38caf4ca7f6eb11e8
Paulina Salem <paulinasalem@yahoo.com>
2018-04-16 00:14:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,5 @@ end
 def assign_rooms(speaker)
   new_array = []
 speaker.each_with_index { |speaker, index| new_array.push("Hello, #{speaker}! You'll be assigned to room #{index}!")}
-end
 new_array
 end
