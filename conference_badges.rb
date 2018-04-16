623e2199f67bc0e0032ecc70314ae8094b865f69
Paulina Salem <paulinasalem@yahoo.com>
2018-04-16 00:55:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,5 +20,5 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |attendee|
     puts batch_badge_creator(attendee)
   end
-  puts assign_rooms(attendees)
+  #puts assign_rooms(attendees)
 end
